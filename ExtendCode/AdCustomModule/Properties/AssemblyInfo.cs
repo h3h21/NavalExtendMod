054ef3b5a6f6016cb687eea98b46af100daf2219
h3h21 <123787424+h3h21@users.noreply.github.com>
2023-09-09 15:00:00 +0800

    ExtendBesiege
    ExtendBesiege

---
+++ ExtendCode/AdCustomModule/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("AdCustomModule")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AddCustomModuleMod")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("A033CF51-D84F-45DE-B9A9-DEF1ED9A6075")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
